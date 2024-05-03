7896dd8319c423365021c7ee9b7cafcbe90b5a6a
Cássia Oliveira dos Santos <86209186+CassiaSantos@users.noreply.github.com>
2024-05-03 18:51:53 -0300

    Update Main.java

--- Dominando a linguagem de programacao JAVA/Main.java
+++ Dominando a linguagem de programacao JAVA/Main.java
@@ -1,6 +1,11 @@
-/*Introdução: */
-public class Main{
+/* nomes de classes sempre iniciam pela letra maiúscula*/
+
+public class NomeDaClasse{
+
+    //método que tem capacidade de inicialização - main();
+    //método main() recebe como parâmetros um array de String;
+    //método sem retorno: void
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Primeiro código Java do Bootcamp Santader Java Back-end 2024!");
 	}
 }
