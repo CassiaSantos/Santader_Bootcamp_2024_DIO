dc7d65007d5891ed7f0f90118f3bea89322d3bbb
Cássia Oliveira dos Santos <86209186+CassiaSantos@users.noreply.github.com>
2024-05-20 13:25:16 -0300

    edição inicial do AboutMe.java

--- CodigosDoCurso/Conceitos iniciais/SmartTV/AboutMe.java
+++ CodigosDoCurso/Conceitos iniciais/SmartTV/AboutMe.java
@@ -1,1 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
 
+public class AboutMe {
+	public static void main(String[] args) {
+		
+        //cria novo obejto: 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.next();
+        
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+        
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+        
+        //Impressões:
+        System.out.println"Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase();
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close(); 
+	}
+}
